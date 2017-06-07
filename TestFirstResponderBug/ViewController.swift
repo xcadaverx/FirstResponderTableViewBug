a1612197840bdc6e1527a343a05ae961892780a6
Daniel Williams <dwilliams@iscribehealth.com>
2017-06-07 09:02:18 -0700

    Initial Commit

---
+++ TestFirstResponderBug/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TestFirstResponderBug
+//
+//  Created by Daniel Williams on 6/7/17.
+//  Copyright © 2017 Daniel Williams. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
